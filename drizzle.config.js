3bb1af0b6229313a107f70047311104dc13ad48c
Aakarsh Singh <aakarshshrey12@gmail.com>
2025-02-26 23:08:59 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,8 +3,8 @@ export default {
   schema: "./utils/schema.jsx",
   out: "./drizzle",
   dbCredentials: {
-    url: "postgresql://mapdb_owner:LXjoC68lxHdV@ep-wispy-rice-a5ypl4so.us-east-2.aws.neon.tech/mapdb?sslmode=require",
+    url: "postgresql://mapdb_owner:LXjoC68lxHdV@ep-wispy-rice-a5ypl4so-pooler.us-east-2.aws.neon.tech/mapdb?sslmode=require",
     connectionString:
-      "postgresql://mapdb_owner:LXjoC68lxHdV@ep-wispy-rice-a5ypl4so.us-east-2.aws.neon.tech/mapdb?sslmode=require",
+      "postgresql://mapdb_owner:LXjoC68lxHdV@ep-wispy-rice-a5ypl4so-pooler.us-east-2.aws.neon.tech/mapdb?sslmode=require",
   },
 };
